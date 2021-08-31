c26ea0b710aa17ead3aafd776a2e6ecc2cca377e
kiryl-f <67046881+kiryl-f@users.noreply.github.com>
2021-08-31 13:45:40 +0300

    Update MovieService.java

--- app/src/main/java/com/kirvelstudios/cinemaquiz/Retrofit/MovieService.java
+++ app/src/main/java/com/kirvelstudios/cinemaquiz/Retrofit/MovieService.java
@@ -5,6 +5,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface MovieService {
-    @GET("3/movie/top_rated?api_key=33d65e0ed0777308653502b72db75fd0&language=en-US&region=US&page=")
+    @GET("3/movie/top_rated?api_key=key&language=en-US&region=US&page=")
     Call<Result> getMovie(@Query("page")int page, @Query("api_key")String apiKey, @Query("language")String language, @Query("region")String region);
 }
